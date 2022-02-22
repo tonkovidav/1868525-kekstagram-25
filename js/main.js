51d4ae37bfb8b91c35ab2d5ca0edb819d5b7f835
tonkovidav <90862589+tonkovidav@users.noreply.github.com>
2022-02-22 13:16:32 +0200

    Update 22.02

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// function getFromTo(min, max) {
+//   let myMin;
+//   let myMax;
+
+//   if (min < 0) {
+//     myMin = 0;
+//   } else {
+//     myMin = min;
+//   }
+
+//   if (max < 0 || max < min) {
+//     myMax = 10;
+//   } else {
+//     myMax = max;
+//   }
+
+//   let rndNum = Math.random(0, 100);
+
+//   while(rndNum < min || rndNum > max) {
+//     if (rndNum >= myMin && rndNum <= myMax) {
+//       return rndNum;
+//       break;
+//     } else {
+//       rndNum = Math.random(0, 100);
+//     }
+//   }
+// }
+
+function getFromTo(min, max) {
+  let myMin;
+  let myMax;
+
+  if (min < 0) {
+    myMin = 0;
+  } else {
+    myMin = min;
+  }
+
+  if (max < 0 || max < min) {
+    myMax = 10;
+  } else {
+    myMax = max;
+  }
+
+  return Math.random(myMin, myMax);
+}
+
+function getStringLength(checkString, maxStringLength) {
+  if (checkString.length <= maxStringLength) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
