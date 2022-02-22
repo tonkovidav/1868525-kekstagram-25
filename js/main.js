69670359aaf3c36cf3443d8d062616cfdeb577e0
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-02-22 16:58:52 +0300

    step 9

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ function ensureMaxIsGreater(myMin, max){
 
 function getFromTo (min, max) {
   const myMin = getPositiveValue(min);
-  let myMax;
+
 
   // правильно, что ты не доверяешь полученным параметрам, а проверяешь их.
   // однако, лучше оператор if-else выковырять в отдельную функцию
@@ -50,11 +50,8 @@ function getFromTo (min, max) {
   // хотя сначала обратим внимание, что myMin, в общем-то может быть больше 10, (11, например).
   // Если это так, то у нас нет возможности правильно обслужить запрос.
   // в таком виде мы можем унести правило в отдельную функцию
-  if (max < myMin) {
-    throw new Error(`неправильные аргументы. договорились, что min>0 и max> min, а получили ${min}, ${max}`);
-  } else {
-    myMax = max;
-  }
+  // а переменную myMax сделать неизменяемой
+  const myMax = ensureMaxIsGreater(myMin, max);
 
   return Math.random(myMin, myMax);
 }
