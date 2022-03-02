5d36c4961843c053c6b9d295bafb59b3a2db6ed1
tonkovidav <90862589+tonkovidav@users.noreply.github.com>
2022-03-02 17:07:00 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,53 +30,51 @@ window.console.log (checkStringLength (110, 150));
 
 // module4-task1
 
-const id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-
 const url = [
-  photos/'1'.jpg,
-  photos/'2'.jpg,
-  photos/'3'.jpg,
-  photos/'4'.jpg,
-  photos/'5'.jpg,
-  photos/'6'.jpg,
-  photos/'7'.jpg,
-  photos/'8'.jpg,
-  photos/'9'.jpg,
-  photos/'10'.jpg,
-  photos/'11'.jpg,
-  photos/'12'.jpg,
-  photos/'13'.jpg,
-  photos/'14'.jpg,
-  photos/'15'.jpg,
-  photos/'16'.jpg,
-  photos/'17'.jpg,
-  photos/'18'.jpg,
-  photos/'19'.jpg,
-  photos/'20'.jpg,
-  photos/'21'.jpg,
-  photos/'22'.jpg,
-  photos/'23'.jpg,
-  photos/'24'.jpg,
-  photos/'25'.jpg
+  'photos/1.jpg',
+  'photos/2.jpg',
+  'photos/3.jpg',
+  'photos/4.jpg',
+  'photos/5.jpg',
+  'photos/6.jpg',
+  'photos/7.jpg',
+  'photos/8.jpg',
+  'photos/9.jpg',
+  'photos/10.jpg',
+  'photos/11.jpg',
+  'photos/12.jpg',
+  'photos/13.jpg',
+  'photos/14.jpg',
+  'photos/15.jpg',
+  'photos/16.jpg',
+  'photos/17.jpg',
+  'photos/18.jpg',
+  'photos/19.jpg',
+  'photos/20.jpg',
+  'photos/21.jpg',
+  'photos/22.jpg',
+  'photos/23.jpg',
+  'photos/24.jpg',
+  'photos/25.jp'
 ];
 
 const description = [
-'Свадедное фото',
-'Кот в сапогах',
-'Путешествие на край света',
-'Роджество',
-'Встреча однокласников',
-'Мой завтрак',
-'Тренировка'
+  'Свадедное фото',
+  'Кот в сапогах',
+  'Путешествие на край света',
+  'Роджество',
+  'Встреча однокласников',
+  'Мой завтрак',
+  'Тренировка'
 ];
 
 const commentsAvatars = [
-'img/avatar-1.svg',
-'img/avatar-2.svg',
-'img/avatar-3.svg',
-'img/avatar-4.svg',
-'img/avatar-5.svg',
-'img/avatar-6.svg'
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg'
 ];
 
 const messages = [
@@ -86,7 +84,7 @@ const messages = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-]
+];
 
 const profileNames = [
   'Иван',
@@ -110,8 +108,7 @@ const createComment = () => {
     message: getArrayElements (messages),
     name: getArrayElements (profileNames)
   }
-
-}
+};
 
 const createPost = () => {
   return {
@@ -119,6 +116,7 @@ const createPost = () => {
     url: getArrayElements (url),
     description: getArrayElements (description),
     likes: getRandomPositiveInteger (15,200),
-    comments: getArrayElements (comments)
+    comments: createComment ()
   }
-}
+};
+createPost ();
