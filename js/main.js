f7e53622760f1cbc43215cddf061a71d7f5e6e33
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-11 16:53:35 +0300

    creates getImageElementFromTemplate

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@ import {createPost} from './data.js';
 const IMAGE_TEMPLATE_ID = '#picture';
 const IMAGE_ITEM_SELECTOR = '.picture';
 const getImageElementFromTemplate = ()=>{
-  const template = document.querySelector(IMAGE_TEMPLATE_ID).content.querySelector(IMAGE_ITEM_SELECTOR);
+  /**
+   * @type {HTMLTemplateElement}
+   */
+  const template = document.querySelector(IMAGE_TEMPLATE_ID);
   if(!template){
     throw new Error('Template not found');
   }
-  return template;
+  /**
+   * @type {HTMLAnchorElement}
+   */
+  const element = template.content.querySelector(IMAGE_ITEM_SELECTOR);
+  if(!element){
+    throw new Error('Element not found');
+  }
+  return element.cloneNode(true);
 };
 createPost ();
