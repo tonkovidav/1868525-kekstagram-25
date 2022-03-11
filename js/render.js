ef9ebacf77b087e7f7250450f17e71d0c36c4e23
tonkovidav <90862589+tonkovidav@users.noreply.github.com>
2022-03-11 15:31:24 +0200

    Create render.js

---
+++ js/render.js
@@ -0,0 +1,12 @@
+import {createPost} from './data.js';
+const picturesList = document.querySelector('.pictures');
+const userPhotos = createPost (25);
+const template = document.querySelector('#picture').content.querySelector('.picture');
+const photoFragment = document.createDocumentFragment ();
+userPhotos.forEach(({url, likes, comments}) => {
+  const photoElement = template.cloneNode(true);
+  photoElement.querySelector('.picture__img').src = url;
+  photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.querySelector('.picture__comments').textContent = [comments];
+});
+picturesList.appendChild(photoFragment);
