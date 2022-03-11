8b59cc620042f8c31efbe16c67c98038fe546b10
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-11 16:48:41 +0300

    creates function to get image element

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import {createPost} from './data.js';
 const IMAGE_TEMPLATE_ID = '#picture';
 const IMAGE_ITEM_SELECTOR = '.picture';
-
+const getImageElementFromTemplate = ()=>{
+  const template = document.querySelector(IMAGE_TEMPLATE_ID).content.querySelector(IMAGE_ITEM_SELECTOR);
+  if(!template){
+    throw new Error('Template not found');
+  }
+  return template;
+};
 createPost ();
